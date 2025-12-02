a409df7056e13c1398622c2d36d3c30c4ada7fd8
MateAmargo <pierotti.esteban@gmail.com>
2025-12-01 20:41:33 -0500

    update day17 project

--- [Day 17:] The Quiz Project & The Benefits of OOP/Day17Project_Quiz/data.py
+++ [Day 17:] The Quiz Project & The Benefits of OOP/Day17Project_Quiz/data.py
@@ -1,16 +1,113 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-             "to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral."
-        , "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "Entertainment: Music",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "False"
+            ],
+        "question": "Rapper Snoop Dogg&#039;s real name is &#039;Cordozar Calvin Broadus, Jr.&#039;.",
+        "type": "boolean"
+        },
+        {
+        "category": "Entertainment: Music",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "False"
+            ],
+        "question": "The cover of The Beatles album &quot;Abbey Road&quot; featured a Volkswagen Beetle in the background.",
+        "type": "boolean"
+        },
+        {
+        "category": "Sports",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "False"
+            ],
+        "question": "Skateboarding will be included in the 2020 Summer Olympics in Tokyo.",
+        "type": "boolean"
+        },
+        {
+        "category": "Entertainment: Video Games",
+        "correct_answer": "False",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "True"
+            ],
+        "question": "In &quot;Team Fortress 2&quot;, the &quot;Bill&#039;s Hat&quot; is a reference to the game &quot;Dota 2&quot;.",
+        "type": "boolean"
+        },
+        {
+        "category": "Entertainment: Video Games",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "False"
+            ],
+        "question": "In the game &quot;Subnautica&quot;, a &quot;Cave Crawler&quot; will attack you.",
+        "type": "boolean"
+        },
+        {
+        "category": "Entertainment: Film",
+        "correct_answer": "False",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "True"
+            ],
+        "question": "Joan Cusack starred in the 2009 disaster movie, &quot;2012&quot;.",
+        "type": "boolean"
+        },
+        {
+        "category": "Entertainment: Television",
+        "correct_answer": "False",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "True"
+            ],
+        "question": "Bob Ross was a US Air Force pilot.",
+        "type": "boolean"
+        },
+        {
+        "category": "Entertainment: Video Games",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "False"
+            ],
+        "question": "The character that would eventually become Dr. Eggman was considered for the role of Sega&#039;s new flagship mascot before Sonic was.",
+        "type": "boolean"
+        },
+        {
+        "category": "Science & Nature",
+        "correct_answer": "False",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "True"
+            ],
+        "question": "Sound can travel through a vacuum.",
+        "type": "boolean"
+        },
+        {
+        "category": "Geography",
+        "correct_answer": "True",
+        "difficulty": "medium",
+        "incorrect_answers":
+            [
+            "False"
+            ],
+        "question": "The title of the 1969 film &quot;Krakatoa, East_of Java&quot; is incorrect, as Krakatoa is in fact west of Java.",
+        "type": "boolean"
+        }
 ]
+
--- [Day 17:] The Quiz Project & The Benefits of OOP/Day17Project_Quiz/question_model.py
+++ [Day 17:] The Quiz Project & The Benefits of OOP/Day17Project_Quiz/question_model.py
@@ -1,6 +1,7 @@
 
 class Question:
-    def __init__(self, text, answer):
+    def __init__(self, text, answer, category):
         self.text = text
         self.answer = answer
+        self.category = category
 
